665bf81f705f65c8b49715b5c09c72e57f895cd5
ylfyt <ayudi3521@gmail.com>
2023-04-05 07:48:34 +0700

    Adding android

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,26 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>The Next Gen Email</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				<IonHeader collapse="condense">
+					<IonToolbar>
+						<IonTitle size="large">The Next Gen Email</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<div className="container">
+					<strong>Hello, World 👋</strong>
+				</div>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Home;
