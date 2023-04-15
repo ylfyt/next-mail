50349b574eb210b0f7ae218d9966dad5fd07e552
ylfyt <ayudi3521@gmail.com>
2023-04-16 02:13:47 +0700

    Fix Issue vite base url when building for android

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/the-next-gen-email'
+export default defineConfig(({mode}) => {
+  console.log(`Building the app with ${mode} mode...`);
+  return {
+    plugins: [react()],
+    base: mode === 'production:android' ? '/' : '/the-next-gen-email'
+  }
 })
